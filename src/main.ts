f5e0df8af561c0b8bfb8177ce0967e9cc73db5fd
yukang <moorekang@gmail.com>
2022-07-11 13:59:47 +0800

    finish v0.0.1

--- src/main.ts
+++ src/main.ts
@@ -6,76 +6,135 @@ import {
 	Plugin,
 	PluginSettingTab,
 	Setting,
+	EditorPosition,
 } from "obsidian";
 
-
-import LanguageCode from "./langCode";
-import langCodes from "./langCodes";
-
-const TRANSLATE_API_URL = "https://api.cognitive.microsofttranslator.com";
-const AUTH_URL =
-	"https://func-language-worker-auth.azurewebsites.net/api/GetAuthToken";
-const MAX_CHARACTERS = 1000;
-
 interface LanguageTranslatorSettings {
-    defaultLanguage: LanguageCode;
+	defaultLanguage: string;
 }
 
 const DEFAULT_SETTINGS: LanguageTranslatorSettings = {
-    defaultLanguage: {
-		text: "English",
-		code: "en",
-	}
+	defaultLanguage: "Microsoft David - English (United States)"
 };
+
+
+async function getNextAudio(sentence: string, code: string) {
+	console.log(sentence);
+	let voice = speechSynthesis.getVoices().find(v => v.name == code);
+
+	let audio = new SpeechSynthesisUtterance(sentence);
+	let englishCharsCount = Array.from(sentence).map(c => (c.match(/[a-zA-Z]/) ? 1 : 0));
+	let englishCharsSum = englishCharsCount.reduce(function (sum, cur) { return sum + cur }, 0);
+	if (englishCharsSum / sentence.length > 0.65) {
+		voice = speechSynthesis.getVoices().find(v => v.lang == "en-US");
+	}
+	if (voice) {
+		audio.lang = voice.lang;
+		audio.voice = voice;
+	}
+	window.speechSynthesis.speak(audio);
+
+	return new Promise(resolve => {
+		audio.onend = resolve;
+	});
+}
 
 export default class LanguageTranslator extends Plugin {
 	settings: LanguageTranslatorSettings;
 	token: string;
-    instance: SpeechSynthesisUtterance;
+	instance: SpeechSynthesisUtterance;
+	speaking: boolean = false;
+	cursor: EditorPosition | null = null;
 
-	onEditorCallback = async (editor: Editor) => {
+	// cleanup text
+	cleanUpText = (text: string) => {
+		let res = text.replace(/\[([^\[\]]*)\]\((.*?)\)/gm, '$1');
+		//remove # from the beginning of the line
+		res = res.replace(/^#/gm, '');
+		//remove http url from text
+		res = res.replace(/\(http(.*?)\)/gm, '');
+		//remove markdown images from text
+		res = res.replace(/!\[(.*?)\]\((.*?)\)/gm, '').replace(/!\[\[(.*?)\]\]/gm, '');
+		return res;
+	}
+
+	// Speech from current line
+	SpeechFromCurrentLine = async (editor: Editor) => {
+		this.cursor = editor.getCursor();
+		this.StartSpeech(editor);
+	}
+
+	StartSpeech = async (editor: Editor) => {
 		try {
-			const selection = editor.getSelection();
-			if (selection.length > MAX_CHARACTERS) {
-				new Notice(`Exceeded ${MAX_CHARACTERS} max characters!`);
-				return;
+			let selection = editor.getSelection();
+			let cursor = editor.getCursor();
+
+			// if no selection, use the whole text
+			if (selection.length === 0) {
+				if (this.cursor) {
+					let lastLine = editor.getLine(editor.lastLine());
+					let endEndCursor = { line: editor.lastLine(), ch: lastLine.length };
+					selection = editor.getRange(this.cursor, endEndCursor);
+					cursor = this.cursor;
+				} else {
+					selection = editor.getValue();
+					cursor = { line: 0, ch: 0 };
+				}
 			}
 
-			let textForSpeech = selection;
-			let targetLang = this.settings.defaultLanguage.code;
-            console.log("textForTranslation: {}", textForSpeech);
-            var msg = new SpeechSynthesisUtterance();
-            msg.text = textForSpeech;
-            msg.lang = this.settings.defaultLanguage.code;
-            window.speechSynthesis.speak(msg);
+			let lines = selection.split('\n');
+			this.speaking = true;
+
+			//try to fix cursor
+			if (lines.length > 0) {
+				let line = lines[0];
+				let allLine = editor.getLine(cursor.line);
+				let cursorLine = editor.getRange(cursor, { line: cursor.line, ch: allLine.length }).replace(/\n/g, '');
+				if (line != cursorLine) {
+					//means cursor is at the end of selection, try to fix it.
+					cursor = { line: cursor.line - lines.length + 1, ch: 0 };
+				}
+			}
+
+			for (let i = 0; i < lines.length; i++) {
+				if (!this.speaking) {
+					continue;
+				}
+				let textForSpeech = lines[i];
+
+
+				//if the line is all English words, set code to en
+				textForSpeech = this.cleanUpText(textForSpeech);
+
+
+				let nextCursor = {
+					line: cursor.line + i,
+					ch: 0,
+				};
+				editor.setCursor(nextCursor);
+				await getNextAudio(textForSpeech, this.settings.defaultLanguage);
+
+				this.cursor = null;
+			};
 		} catch (err) {
 			console.log(err);
 			new Notice(err.message);
 		}
 	};
 
-    onCancelCallback = async (editor: Editor) => {
-        window.speechSynthesis.cancel();
-    }
-
-	getToken = async () => {
-		try {
-			const response = await fetch(AUTH_URL);
-			this.token = await response.text();
-		} catch (err) {
-			console.log(err);
-			new Notice(err.message);
-		}
-	};
+	onCancelCallback = async (editor: Editor) => {
+		console.log("cancel speak");
+		this.speaking = false;
+		window.speechSynthesis.cancel();
+	}
 
 	async onload() {
 		await this.loadSettings();
-		await this.getToken();
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: "editor-say-it-start-command",
-			name: "Speak out the text",
-			editorCallback: this.onEditorCallback,
+			name: "Speech: Speech the text",
+			editorCallback: this.StartSpeech,
 			hotkeys: [
 				{
 					modifiers: ["Ctrl", "Shift"],
@@ -84,10 +143,22 @@ export default class LanguageTranslator extends Plugin {
 			],
 		});
 
-        this.addCommand({
+		this.addCommand({
+			id: "editor-say-it-start-from-current-pos-command",
+			name: "Speech: Speech the text from current pos",
+			editorCallback: this.SpeechFromCurrentLine,
+			hotkeys: [
+				{
+					modifiers: ["Ctrl", "Shift"],
+					key: "C",
+				},
+			],
+		});
+
+		this.addCommand({
 			id: "editor-say-it-stop-command",
-			name: "Cancel speak",
-			editorCallback: this.onEditorCallback,
+			name: "Speech: Cancel ongoin speech",
+			editorCallback: this.onCancelCallback,
 			hotkeys: [
 				{
 					modifiers: ["Ctrl", "Shift"],
@@ -115,10 +186,18 @@ export default class LanguageTranslator extends Plugin {
 
 class LanguageTranslatorSettingsTab extends PluginSettingTab {
 	plugin: LanguageTranslator;
+	allLanguages: string[];
 
 	constructor(app: App, plugin: LanguageTranslator) {
 		super(app, plugin);
 		this.plugin = plugin;
+		this.allLanguages = [
+			'Microsoft David - English (United States)',
+			'Microsoft Mark - English (United States)',
+			'Microsoft Zira - English (United States)',
+			'Microsoft Huihui - Chinese (Simplified, PRC)',
+			'Microsoft Kangkang - Chinese (Simplified, PRC)',
+			'Microsoft Yaoyao - Chinese (Simplified, PRC)']
 	}
 
 	display(): void {
@@ -132,17 +211,15 @@ class LanguageTranslatorSettingsTab extends PluginSettingTab {
 			.setName("Default Language")
 			.setDesc("Set default language for speak out")
 			.addDropdown((dropDown) => {
-				langCodes.forEach((el) => {
-					dropDown.addOption(el.code, el.text);
+				console.log(this.allLanguages);
+				this.allLanguages.forEach((el) => {
+					dropDown.addOption(el, el);
 				});
 				dropDown.onChange(async (value) => {
-                    var lang = langCodes.find(x => x.code == value);
-                    if (lang) {
-                        this.plugin.settings.defaultLanguage = lang;
-                    }
+					this.plugin.settings.defaultLanguage = value;
 					await this.plugin.saveSettings();
 				});
-				dropDown.setValue(this.plugin.settings.defaultLanguage.code);
+				dropDown.setValue(this.plugin.settings.defaultLanguage);
 			});
 	}
 }
